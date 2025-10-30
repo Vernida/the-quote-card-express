58e6e8cdaf42b8766bb0175f3b29400db0f7b6d4
Vernida <vernida.designs@gmail.com>
2025-10-30 01:55:40 -0400

    Removed the quote loop function and setup a request to the Unsplash API for a random background image

--- public/js/index.js
+++ public/js/index.js
@@ -6,38 +6,55 @@ const elements = {
     source: document.getElementById("source"),
 };
 
-const quotes = [
-    {
-        quote: "All hands! Abandon ship!",
-        author: "Captain Picard",
-        source: "Star Trek: The Next Generation",
-    },
+async function getRandomImage() {
+    const client_id = "YOUR_ACCESS_KEY";
+    const endpoint = `https://api.unsplash.com/photos/random/?client_id=${client_id}`;
+    try {
+        const response = await fetch(endpoint);
+        const returnedData = await response.json();
+        const receivedPhotoUrl = returnedData.urls.regular;
 
-    {
-        quote: "Doh!",
-        author: "Homer Simpson",
-        source: "The Simpsons",
-    },
-
-    {
-        quote: "...it ’s no use going back to yesterday, because I was a different person then.",
-        author: "Lewis Carroll",
-        source: "Alice's Adventures in Wonderland",
+        const imgDiv = document.querySelector(".background-img");
+        imgDiv.style.backgroundImage = `url("${receivedPhotoUrl}")`;
+    } catch (error) {
+        console.error(error);
     }
-]
-
-function loopThroughQuotes() {
-    let quoteIndex = 0;
-    setInterval(() => {
-        if (quoteIndex < quotes.length) {
-            elements.quote.textContent = quotes[quoteIndex].quote;
-            elements.author.textContent = quotes[quoteIndex].author;
-            elements.source.textContent = quotes[quoteIndex].source;
-            quoteIndex++;
-        } else {
-            quoteIndex = 0;
-        }
-    }, 3000)
 }
 
-setTimeout(loopThroughQuotes, 3000);
+getRandomImage();
+
+// const quotes = [
+//     {
+//         quote: "All hands! Abandon ship!",
+//         author: "Captain Picard",
+//         source: "Star Trek: The Next Generation",
+//     },
+
+//     {
+//         quote: "Doh!",
+//         author: "Homer Simpson",
+//         source: "The Simpsons",
+//     },
+
+//     {
+//         quote: "...it ’s no use going back to yesterday, because I was a different person then.",
+//         author: "Lewis Carroll",
+//         source: "Alice's Adventures in Wonderland",
+//     }
+// ]
+
+// function loopThroughQuotes() {
+//     let quoteIndex = 0;
+//     setInterval(() => {
+//         if (quoteIndex < quotes.length) {
+//             elements.quote.textContent = quotes[quoteIndex].quote;
+//             elements.author.textContent = quotes[quoteIndex].author;
+//             elements.source.textContent = quotes[quoteIndex].source;
+//             quoteIndex++;
+//         } else {
+//             quoteIndex = 0;
+//         }
+//     }, 3000)
+// }
+
+// setTimeout(loopThroughQuotes, 3000);
